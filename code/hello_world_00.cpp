7a7a9c8bd0d92a7bd711c034393faaa422b1cd02
Daniel Schuette <d.schuette@online.de>
2018-10-02 16:23:55 +0200

    add another hello world program

---
+++ code/hello_world_00.cpp
@@ -0,0 +1,13 @@
+// a simple `hello world' program written in C++
+//
+// author: Daniel Schuette
+// date: 2018/09/14
+// license: MIT
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, world" << endl;
+    return 0;
+}
---
+++ code/hello_world_01.cpp
@@ -0,0 +1,14 @@
+// this program demonstrates that in programming,
+// the same task can oftentimes be accomplished in 
+// multiple different ways
+// compare this `hello world' program to `./hello_world_00.cpp'
+//
+// author: Daniel Schuette
+// date: 2018/10/02
+// license: MIT
+#include <cstdio> /* this program uses the C standard library */
+
+int main() {
+    puts("hello world");
+    return 0;
+}
